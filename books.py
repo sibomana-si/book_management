97fe778037d3ac4a6b570e894686e5e88d384850
simon <si.sibomana@gmail.com>
2026-02-12 21:50:14 +0200

    add endpoint for create_book post request

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -43,4 +43,9 @@ async def read_author_category_by_query(book_author: str, category: str):
             book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
+
+
+@app.post("/books/create_book/")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
 
