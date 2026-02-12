63cbaf843ed6c225f032f21903dd617fd961163d
simon <si.sibomana@gmail.com>
2026-02-12 21:36:02 +0200

    add the endpoints for read_category_by_query and read_author_category_by_query

--- books.py
+++ books.py
@@ -25,3 +25,22 @@ async def read_book(book_title: str):
             return book
     return {'message': 'Book not found'}
 
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
